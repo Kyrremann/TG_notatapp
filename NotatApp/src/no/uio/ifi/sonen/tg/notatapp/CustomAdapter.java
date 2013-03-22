bdbc78ea8fb4b2d5e41919f51e164ffdc48dd770
Kyrremann <Kyrremann@gmail.com>
2013-03-22 22:43:20 +0100

    First commit with the simple version of \!atApp, enjoy\!

---
+++ NotatApp/src/no/uio/ifi/sonen/tg/notatapp/CustomAdapter.java
@@ -0,0 +1,52 @@
+package no.uio.ifi.sonen.tg.notatapp;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class CustomAdapter extends ArrayAdapter<Note> {
+
+	ArrayList<Note> notes;
+	int textViewResourceId;
+	Context context;
+
+	public CustomAdapter(Context context, int textViewResourceId,
+			ArrayList<Note> notes) {
+		super(context, textViewResourceId, notes);
+		this.notes = notes;
+		this.textViewResourceId = textViewResourceId;
+		this.context = context;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		Holder holder = null;
+
+		if (convertView == null) {
+			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+			convertView = inflater.inflate(textViewResourceId, parent, false);
+			
+			holder = new Holder();
+			holder.title = (TextView) convertView.findViewById(R.id.note_title);
+			convertView.setTag(holder);
+		} else {
+			holder = (Holder) convertView.getTag();
+		}
+		
+		holder.title.setText(position + ": " + notes.get(position).title); 
+
+		return convertView;
+	}
+
+	private class Holder {
+		TextView title;
+	}
+
+}
---
+++ NotatApp/src/no/uio/ifi/sonen/tg/notatapp/Note.java
@@ -0,0 +1,12 @@
+package no.uio.ifi.sonen.tg.notatapp;
+
+public class Note {
+	
+	public static final String TITLE = "title", NOTE = "note"; 
+	public String title, note;
+
+	public Note(String title, String note) {
+		this.title = title;
+		this.note = note;
+	}
+}
